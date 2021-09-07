166df79d1b0af789b20c2230640a42c4707f3598
annikamarriner <89063765+annikamarriner@users.noreply.github.com>
2021-09-07 20:55:27 +1200

    create javascript
    added javascript to allow for airtable to be used in future events.

--- VUE.js
+++ VUE.js
@@ -1,3 +1,35 @@
 var app = new Vue({
     el: '#app', // this must match the name of the div in html
+})    data: {
+
+        items: [], //an array to hold all of the items it gets from the database 
+
+    },
+    mounted: function() {
+        this.loadItems();
+    },
+    methods: {
+        loadItems: function() {
+
+            //Init variables - these are in your airtable
+            var self = this
+            var app_id = "hghghghg";
+            var app_key = "hhhghghgh";
+            this.items = []
+
+            //make sure to have the correct label name here
+            axios.get(
+                "https://api.airtable.com/v0/" + app_id + "fjhghg",
+                {
+                    headers: { Authorization: "Bearer " + app_key }                
+                }
+
+            ).then(function(response) {
+                self.items = response.data.records
+            }).catch(function(error){
+                console.log(error)
+            })
+        }
+    }
 })
+
