a255f9213cb8030bd2be22ff58bd2a165dc61852
annikamarriner <89063765+annikamarriner@users.noreply.github.com>
2021-09-07 20:56:34 +1200

    add javascript to allow "sticky" navigation

--- VUE.js
+++ VUE.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app', // this must match the name of the div in html
-})    data: {
+
+    data: {
 
         items: [], //an array to hold all of the items it gets from the database 
 
@@ -33,3 +34,23 @@ var app = new Vue({
     }
 })
 
+
+
+// javascript for sticky top to enable the navigation bar to be visible while scrolling down the pages//
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navigation = document.getElementById("navigation");
+
+// Get the offset position of the navbar
+var sticky = navigation.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navigation.classList.add("sticky")
+  } else {
+    navigation.classList.remove("sticky");
+  }
+}
