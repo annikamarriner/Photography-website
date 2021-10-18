81d8c210fa4775af68054b17fc2e099ef492ab88
annikamarriner <89063765+annikamarriner@users.noreply.github.com>
2021-10-19 09:56:47 +1300

    update pages

--- VUE.js
+++ VUE.js
@@ -1,38 +1,38 @@
-var app = new Vue({
-    el: '#app', // this must match the name of the div in html
+// var app = new Vue({
+//     el: '#app', // this must match the name of the div in html
 
-    data: {
+//     data: {
 
-        items: [], //an array to hold all of the items it gets from the database 
+//         items: [], //an array to hold all of the items it gets from the database 
 
-    },
-    mounted: function() {
-        this.loadItems();
-    },
-    methods: {
-        loadItems: function() {
+//     },
+//     mounted: function() {
+//         this.loadItems();
+//     },
+//     methods: {
+//         loadItems: function() {
 
-            //Init variables - these are in your airtable
-            var self = this
-            var app_id = "hghghghg";
-            var app_key = "hhhghghgh";
-            this.items = []
+//             //Init variables - these are in your airtable
+//             var self = this
+//             var app_id = "hghghghg";
+//             var app_key = "hhhghghgh";
+//             this.items = []
 
-            //make sure to have the correct label name here
-            axios.get(
-                "https://api.airtable.com/v0/" + app_id + "fjhghg",
-                {
-                    headers: { Authorization: "Bearer " + app_key }                
-                }
+//             //make sure to have the correct label name here
+//             axios.get(
+//                 "https://api.airtable.com/v0/" + app_id + "fjhghg",
+//                 {
+//                     headers: { Authorization: "Bearer " + app_key }                
+//                 }
 
-            ).then(function(response) {
-                self.items = response.data.records
-            }).catch(function(error){
-                console.log(error)
-            })
-        }
-    }
-})
+//             ).then(function(response) {
+//                 self.items = response.data.records
+//             }).catch(function(error){
+//                 console.log(error)
+//             })
+//         }
+//     }
+// })
 
 
 
@@ -41,7 +41,7 @@ var app = new Vue({
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navigation = document.getElementById("navigation");
+var navigation = document.querySelector(".navigation");
 
 // Get the offset position of the navbar
 var sticky = navigation.offsetTop;
@@ -53,4 +53,40 @@ function myFunction() {
   } else {
     navigation.classList.remove("sticky");
   }
+}
+
+
+//Javascript to allow for drop down navigation to link to other html pages//
+var ddOurWork = document.querySelector(".dropdown--OurWork");
+ddOurWork.onclick = () => {
+	document.location.href = "ourwork.html";
+}
+
+//Javascript to allow for drop down navigation to link to other html pages//
+var ddPricing = document.querySelector(".dropdown--Pricing");
+ddPricing.onclick = () => {
+	document.location.href = "pricing.html";
+}
+
+
+//icon when responsive changes to mobile device//
+function openMobileNav() {
+    var topNav = document.querySelector(".topnav");
+    if (topNav.className === "topnav") {
+        topNav.classList.add("mobile-nav");  
+  }
+    else {
+        topNav.classList.remove("mobile-nav");
+    }
+  }
+
+
+  function expand() {
+    const nav = document.querySelector(".navigation")
+    if (nav.className === "navigation") {
+        nav.classList.add("responsive")
+    }
+    else {
+        nav.classList.remove("responsive")
+    }
 }
