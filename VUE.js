7bd73096e23e054703d52fb7066dfbd3bbfb0cad
annikamarriner <89063765+annikamarriner@users.noreply.github.com>
2021-10-30 21:28:20 +1300

    update website 30/10/21

--- VUE.js
+++ VUE.js
@@ -1,40 +1,58 @@
-// var app = new Vue({
-//     el: '#app', // this must match the name of the div in html
+var app = new Vue({
+  el: '#app', //this must match the name of the div in your html
 
-//     data: {
+  data: {
 
-//         items: [], //an array to hold all of the items it gets from the database 
+      items: [], //an array to hold all the items it gets from the database
 
-//     },
-//     mounted: function() {
-//         this.loadItems();
-//     },
-//     methods: {
-//         loadItems: function() {
+  },
+  mounted: function () {
+      this.loadItems();
+  },
+  methods: {
+      loadItems: function () {
 
-//             //Init variables - these are in your airtable
-//             var self = this
-//             var app_id = "hghghghg";
-//             var app_key = "hhhghghgh";
-//             this.items = []
+          // Init variables - these are in your airtable
+          var self = this
+          var app_id = "appbdqnkQFbiMRoXS";
+          var app_key = "keyQA6oWJrjzUmGUy";
+          this.items = []
 
-//             //make sure to have the correct label name here
-//             axios.get(
-//                 "https://api.airtable.com/v0/" + app_id + "fjhghg",
-//                 {
-//                     headers: { Authorization: "Bearer " + app_key }                
-//                 }
+          //make sure to have the correct table name here
+          axios.get(
+              "https://api.airtable.com/v0/" + app_id + "/favourites?view=Grid%20view",
+              {
+                  headers: { Authorization: "Bearer " + app_key }
+              }
+          ).then(function (response) {
+              self.items = response.data.records
+          }).catch(function (error) {
+              console.log(error)
+          })
+      }
 
-//             ).then(function(response) {
-//                 self.items = response.data.records
-//             }).catch(function(error){
-//                 console.log(error)
-//             })
-//         }
-//     }
-// })
+  },
 
 
+  //computed functions let us get a specific subset from the database that matches one of the fields 
+  computed: {
+
+      filteredRatings: function () {
+          return this.items.filter(function (filteredItem) {
+              return filteredItem.fields.reviewRating == 5
+
+          })
+      },
+
+      filteredYears: function () {
+          return this.items.filter(function (filteredItem) {
+              return filteredItem.fields.year == 2019;
+
+          })
+      }
+  }
+
+})
 
 // javascript for sticky top to enable the navigation bar to be visible while scrolling down the pages//
 // When the user scrolls the page, execute myFunction
@@ -63,9 +81,9 @@ ddOurWork.onclick = () => {
 }
 
 //Javascript to allow for drop down navigation to link to other html pages//
-var ddPricing = document.querySelector(".dropdown--Pricing");
-ddPricing.onclick = () => {
-	document.location.href = "pricing.html";
+var ddContactUs = document.querySelector(".dropdown--ContactUs");
+ddContactUs.onclick = () => {
+	document.location.href = "contact-us.html";
 }
 
 
